718a51496b30c9a48493651b01726dbac78fa886
ryanzhu004 <ryanzhu.1@outlook.com>
2021-03-23 12:09:45 -0400

    skeleton PhysicalObject

---
+++ Towhoe 6/src/PhysicalObject.java
@@ -0,0 +1,34 @@
+import java.awt.Image;
+
+public abstract class PhysicalObject {
+	int xPos, yPos, xVel, yVel; 
+	//(x,y) position with [x,y] vector
+	int hitboxRadius;
+	Image sprite; // not sure how this is going to work for nwo
+	public PhysicalObject() {
+		// no params spawn in at 0,0 with [0,0]
+		xPos=yPos=xVel=yVel=hitboxRadius=0;
+	}
+	public PhysicalObject(int xPos, int yPos, int xVel, int yVel, int hitboxRadius) {
+		// (xPos,yPos) [xVel,yVel]
+		this.xPos=xPos;
+		this.yPos=yPos;
+		this.xVel=xVel;
+		this.yVel=yVel;
+		this.hitboxRadius=hitboxRadius;
+	}
+	public boolean intersect(PhysicalObject o) {
+		// if distance < sum of radii, then intersect
+		return distance(o) < this.hitboxRadius + o.hitboxRadius;	
+	}
+	public double distance(PhysicalObject o) {
+		// distance between two objects
+		return Math.sqrt((this.xPos-o.xPos)*(this.xPos-o.xPos)+(this.yPos-o.yPos)*(this.yPos-o.yPos));
+	}
+	void move() {
+		// no clipping on borders (yet)
+		xPos+=xVel;
+		yPos+=yVel; 
+	}
+	public abstract void draw();
+}
