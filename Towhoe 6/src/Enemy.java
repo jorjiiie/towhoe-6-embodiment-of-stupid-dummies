e84a506410b8be67545092852b9d6b789371b6e6
Theo Krueger <krueger.theo@gmail.com>
2021-03-26 11:04:42 -0400

    mek it work

---
+++ Towhoe 6/src/Enemy.java
@@ -0,0 +1,8 @@
+
+public class Enemy {
+	
+	public Enemy() {
+		
+	}
+
+}
--- Towhoe 6/src/UserPanel.java
+++ Towhoe 6/src/UserPanel.java
@@ -3,21 +3,21 @@ import java.awt.*;
 import java.awt.event.*;
 import java.util.ArrayList;
 
-public class UserPanel extends JPanel implements KeyListener, ActionListener {
+public class UserPanel extends JPanel implements KeyListener, ActionListener, JavaArcade {
 	
 	private Player player; // player
 	private int score, coins; // score, 1 coin = 1 life
 	
 	private javax.swing.Timer timer; // draw rate 
-	private ArrayList<Enemy>; // enemies as arraylist
-
+	private ArrayList<Enemy> enemies; // enemies as arraylist
+	
 	// THE BIG OL CONSTRUCTOR
 	public UserPanel(int width, int height) {
    
 		Color background = Color.black; // fallback, image bg pls
 		score = 0;
 		
-		player = new Player(); // TODO  add args pls
+		player = new Player(width, height); // TODO  add/fix args pls
 		
 		timer = new javax.swing.Timer(16, this); // 16 ms per frame frame rate = ~60, this will stutter a lot noticeably but who cares
 		
@@ -25,7 +25,7 @@ public class UserPanel extends JPanel implements KeyListener, ActionListener {
 
 		setFocusable(true);
 		setFocusTraversalKeysEnabled(false);      
-		setBackground(backColor);
+		setBackground(background);
 	   
 	    setPreferredSize(new Dimension(width, height));
 	}
@@ -55,17 +55,70 @@ public class UserPanel extends JPanel implements KeyListener, ActionListener {
 	}  
 
 	//TODO FIGURE OUT WTF TO DO WITH THIS
-   private void checkStats(){ //called every 5ms, checks status of targets and hero
-   
-   //Set the width in case the panel gets resized.  All enemies share this value, therefore it is
-   //static and its corresponding static method is called using the class name.
-   
-   	 Enemy.setPanelWidth(getWidth()); 
-   	 enemyFast.move();
-   	 enemySlow.move();
-   }
-	   	  
-	   	  
+	  
+	   private void checkStats(){ //called every 5ms, checks status of targets and hero
+	   
+	   //Set the width in case the panel gets resized.  All enemies share this value, therefore it is
+	   //static and its corresponding static method is called using the class name.
+	   
+	   	 // Enemy.setPanelWidth(getWidth()); 
+	   	 // enemyFast.move();
+	   	 // enemySlow.move();
+	   }
+	  public boolean running() {
+	  	return true;
+	  }
+	  
+	  /* This method should start your game, it should also set a global boolean value so that your running method
+	   * can return the appropriate value */
+	   
+	  public void startGame() {
+	  	//start = true; // i hav eno clue
+	  }
+	  
+	  /*This method should return the name of your game */
+	  public String getGameName() {
+	  	return "TOWHOE";
+	  }
+	  
+	  /* This method should stop your timers but save your score, it should set a boolean value to indicate
+	   *the game is not running, this value will be returned by running method */
+	   
+	  public void pauseGame() {
+	  	return;
+	  }
+	  
+	  /* This method should return your instructions */
+	  public String getInstructions() {
+	  	return "ajskld";
+	  }
+	 
+	 /* This method should return the author(s) of the game */
+	  
+	  public String getCredits() {
+	  	return "joe";
+	  }
+	  
+	  /* This method should return the highest score played for this game */
+	  public String getHighScore() {
+	  	return "69420";
+	  }
+	  
+	  /* This method should stop the timers, reset the score, and set a running boolean value to false */
+	  public void stopGame() {
+	  	return;
+	  }
+	  
+	  /* This method shoud return the current players number of points */
+	  
+	  public int getPoints() {
+	  	return 69;
+	}  
+	  /* This method provides access to GameStats display for UserPanel to pass information to update score
+	  GameStats is created in Arcade, a reference should be passed to UserPanel (main panel) to update poionts */
+	  public void setDisplay(GameStats d) {
+	  	return;
+	  }
 	   	 
 }
    
