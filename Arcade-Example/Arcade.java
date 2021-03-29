863c8cc733feb8be63eec05e2e2c29775caf6776
theokrueger <krueger.theo@gmail.com>
2021-03-29 19:11:37 -0400

    fixed bad folder name

---
+++ Arcade-Example/Arcade.java
@@ -0,0 +1,43 @@
+
+
+import java.awt.Container;
+import java.awt.BorderLayout;
+import javax.swing.JPanel;
+import javax.swing.JFrame;
+import javax.swing.border.EmptyBorder;
+
+public class Arcade extends JFrame
+{
+  public Arcade()
+  {
+    super("AP Java Arcade");
+
+    JavaArcade game = new UserPanel(600, 450);
+
+    GameStats display = new GameStats(game); //passing in a JavaArcade, therefore I know I can call getHighScore(), getScore()
+
+		
+    ControlPanel controls = new ControlPanel(game, display); //Also passing in JavaArcade to ControlPanel, I know you will respond to buttons
+    
+    game.setDisplay(display); //provides game ability to update display
+    
+    JPanel panel = new JPanel();
+    panel.setLayout(new BorderLayout());
+    panel.setBorder(new EmptyBorder(0, 5, 0, 5));
+    panel.add(display, BorderLayout.NORTH);
+    panel.add((JPanel)game, BorderLayout.CENTER);
+    panel.add(controls, BorderLayout.SOUTH);
+
+    Container c = getContentPane();
+    c.add(panel, BorderLayout.CENTER);
+  }
+
+  public static void main(String[] args)
+  {
+    Arcade window = new Arcade();
+    window.setBounds(100, 100, 600, 600);
+    window.setDefaultCloseOperation(EXIT_ON_CLOSE);
+    window.setVisible(true);
+  }
+}
+
